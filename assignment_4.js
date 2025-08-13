d6c3714bd133c2daa285c86dc6c694a1027ec8cd
antnose <antnose28@gmail.com>
2025-08-13 22:35:26 +0600

    solve the third problem

--- assignment_4.js
+++ assignment_4.js
@@ -16,3 +16,24 @@ function onlyCharacter(str) {
     return `Invalid`;
   }
 }
+
+function bestTeam(player1, player2) {
+  // You have to write your code here
+  if (typeof (player1 && player2) === "object") {
+    if (
+      player1.cardY + player1.cardR + player1.foul <
+      player2.cardY + player2.cardR + player2.foul
+    ) {
+      return player1.name;
+    } else if (
+      player1.cardY + player1.cardR + player1.foul ===
+      player2.cardY + player2.cardR + player2.foul
+    ) {
+      return `Tie`;
+    } else {
+      return player2.name;
+    }
+  } else {
+    return `Invalid`;
+  }
+}
