d20645f71566ae5c2ac029e7a6a68aa723e43fe7
antnose <antnose28@gmail.com>
2025-08-14 00:18:11 +0600

    solve some issue in code and complete the problem four and five also

--- assignment_4.js
+++ assignment_4.js
@@ -19,7 +19,7 @@ function onlyCharacter(str) {
 
 function bestTeam(player1, player2) {
   // You have to write your code here
-  if (typeof (player1 && player2) === "object") {
+  if (typeof player1 === "object" && typeof player2 === "object") {
     if (
       player1.cardY + player1.cardR + player1.foul <
       player2.cardY + player2.cardR + player2.foul
@@ -36,4 +36,53 @@ function bestTeam(player1, player2) {
   } else {
     return `Invalid`;
   }
+}
+
+function isSame(arr1, arr2) {
+  // You have to write your code here
+
+  if (Array.isArray(arr1) && Array.isArray(arr2)) {
+    if (arr1.length === arr2.length) {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return `Invalid`;
+  }
+}
+
+function resultReport(marks) {
+  // You have to write your code here
+  let obj = {};
+  let pass = 0;
+  let fail = 0;
+  let avg = 0;
+  if (Array.isArray(marks)) {
+    for (let mark of marks) {
+      if (mark > 39) {
+        pass++;
+      } else {
+        fail++;
+      }
+      avg += mark;
+    }
+  } else {
+    return `Invalid`;
+  }
+
+  if (marks.length === 0) {
+    obj["finalScore"] = 0;
+  } else {
+    obj["finalScore"] = Math.round(avg / marks.length);
+  }
+
+  obj["pass"] = pass;
+  obj["fail"] = fail;
+  return obj;
 }
